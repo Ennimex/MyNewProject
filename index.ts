2d32103a54f6da7c868d22994ce3435b1ffbb9d4
Ennimex <169091806+Ennimex@users.noreply.github.com>
2025-02-19 12:03:40 -0600

    Añadir cambios

--- index.ts
+++ index.ts
@@ -3,9 +3,10 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 import Login from './app/screens/Login';
 import Plantilla from './app/screens/Plantilla';
+import Calculadora from './app/components/Calculadora';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Plantilla);
+registerRootComponent(Calculadora);
